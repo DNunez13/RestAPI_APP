3152f1394947a5889e04d43bde01f643562c5a18
Daniel Nunez <daniel.nunez1313@gmail.com>
2022-12-24 22:29:02 -0500

    built out the goal routes and controller. Also added the goal model to the server.js file

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,39 @@
+// @desc Get Goals
+// $route GET /api/goals
+// @access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: 'Get Goals' })
+}
+
+// @desc Set Goal
+// $route POST /api/goals
+// @access Private
+const setGoal = (req, res) => {
+  if(!req.body.text){
+    res.status(400).json({message: 'Please add a text field'})
+  }
+
+  res.status(200).json({message: `Update Goal ${req.params.id}`}) 
+}
+
+// @desc Update Goal
+// $route PUT /api/goals/id
+// @access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({message: `Update Goal ${req.params.id}`})
+}
+
+// @desc Delete Goal
+// $route DELETE /api/goals
+// @access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({message: `Delete Goal ${req.params.id}`}) 
+}
+
+
+module.exports = {
+    getGoals,
+    setGoal, 
+    updateGoal, 
+    deleteGoal
+}
